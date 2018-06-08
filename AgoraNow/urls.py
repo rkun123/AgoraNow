0d4a8c207c3a7ecea58b176c221449a28d603220
rkun123 <tren111213@gmail.com>
2018-06-08 19:15:20 +0900

    Made static folder and set about static files.

--- AgoraNow/urls.py
+++ AgoraNow/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('status/', include("status.urls")),
     path('user/', include("user.urls"))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
